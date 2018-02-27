39a4fdfb24347816bda2e91c20bb4a9ea595f5b0
slyty7 <tyler.j.west.7@gmail.com>
2018-02-27 10:25:38 -0800

    create FinishedList component

---
+++ src/FinishedList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Checkbox from 'material-ui/Checkbox';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+
+
+class FinishedList extends Component {
+
+  render() {
+    return (
+      <div className="finished-list">
+        
+      </div>
+    );
+  }
+}
+
+export default FinishedList;
